382ad078bea239e05855f6a199f38c9c27ff3226
john_neg <john_neg@mail.ru>
2024-06-24 14:28:47 +0300

    web app template fixes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms.fields.simple import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class SearchForm(FlaskForm):
-    search_string = StringField("Данные поля программы")
+    search_string = StringField(
+        "Данные поля программы", validators=[DataRequired("Необходимо ввести запрос")]
+    )
     switch_autocomplete = SubmitField("Автодополнение запроса")
     switch_more_results = SubmitField("Вывести больше результатов")
     search_similar = SubmitField("Поиск")
